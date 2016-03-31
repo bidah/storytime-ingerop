03404946c821052d8e07a009e3d162b48ad574fe
bidah <bartoindahouse@gmail.com>
2016-03-31 11:53:06 -0300

    milestone: added layout of base app to engine, thus keeping css just as before without the engine clutter

--- app/controllers/storytime/blog_homepage_controller.rb
+++ app/controllers/storytime/blog_homepage_controller.rb
@@ -2,6 +2,7 @@ require_dependency "storytime/application_controller"
 
 module Storytime
   class BlogHomepageController < BlogsController
+  layout "layouts/application"
   private
     def load_page
       @page = @current_storytime_site.homepage
